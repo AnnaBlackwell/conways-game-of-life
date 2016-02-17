6b3d7eb6b709e81bd1adbb001a488a7e7350b210
Anna Blackwell <annablackwell@Annas-MacBook-Air.local>
2016-02-17 14:39:00 +1300

    makeNewBoard function is working

--- javascript/components/board.js
+++ javascript/components/board.js
@@ -1,29 +1,24 @@
 
-
-
 var makeNewBoard = function(size) {
-
-	// for (var k = 0; k < size; k++) {
 
 	var divBoard = document.createElement("div")
 	divBoard.id = "board"
-
-	var divRow = document.createElement("div")
-	divRow.id = "row" + [i]
-
-	var divCell = document.createElement("div")
-	divCell.id = "cell" + [i]
-	// }
 
 	console.log(document.getElementById('page'))
 
 	document.getElementById("page").appendChild(divBoard);
 	// console.log(document.getElementById("row"))
 	for (var i = 0; i < size; i++) {
+		var divRow = document.createElement("div")
+		divRow.id = "row" + i
+
 		document.getElementById("board").appendChild(divRow);
 
 		for (var j = 0; j < size; j++) {
-			document.getElementById("row")[i].appendChild(divCell);
+			var divCell = document.createElement("div")
+			divCell.className = "cell" + j
+
+			document.getElementById("row" + i).appendChild(divCell);
 		}
 	}
 }
